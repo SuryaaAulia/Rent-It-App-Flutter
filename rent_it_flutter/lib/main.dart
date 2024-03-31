2d8249f6aecb2a6e76bb496008bd667c393077dd
SuryaaAulia <suryaaulia489@gmail.com>
2024-04-01 03:39:09 +0700

    Add connection HistoryPage
    connect HomePage to HistoryPage

--- rent_it_flutter/lib/main.dart
+++ rent_it_flutter/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:rent_it_flutter/page/history_page.dart';
 import 'package:rent_it_flutter/page/home_page.dart';
 
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: SafeArea(child: HistoryPage()),
+      body: SafeArea(child: HomePage()),
     ),
   ));
 }
