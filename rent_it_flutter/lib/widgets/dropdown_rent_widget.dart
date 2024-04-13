5964df8d5ebe1f24fb0f0d294d00ee27ee5f5eb1
SuryaaAulia <suryaaulia489@gmail.com>
2024-04-13 21:44:38 +0700

    Clean code

--- rent_it_flutter/lib/widgets/dropdown_rent_widget.dart
+++ rent_it_flutter/lib/widgets/dropdown_rent_widget.dart
@@ -28,23 +28,28 @@ class DropdownRentWidget extends StatelessWidget {
           child: Container(
             width: double.infinity,
             height: 50, // Sesuaikan tinggi dengan kebutuhan
-            decoration: redRoundedDropdownDecoration, // Menggunakan dekorasi yang telah dideklarasikan
+            decoration:
+                redRoundedDropdownDecoration, // Menggunakan dekorasi yang telah dideklarasikan
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: DropdownButtonHideUnderline(
                 child: DropdownButton<String>(
                   value: selectedValue,
                   isExpanded: true,
-                  icon: const Icon(Icons.arrow_drop_down, color: Colors.white), // Teks putih
+                  icon: const Icon(Icons.arrow_drop_down,
+                      color: Colors.white), // Teks putih
                   iconSize: 24,
                   elevation: 16,
-                  style: const TextStyle(color: Colors.white, fontSize: 16.0), // Teks putih dengan fontsize 16
-                  dropdownColor: Colors.red, // Warna merah untuk latar belakang item dropdown list
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0), // Teks putih dengan fontsize 16
+                  dropdownColor: Colors
+                      .red, // Warna merah untuk latar belakang item dropdown list
                   onChanged: onChanged,
                   items: <String>[
-                    'Gedung Serbaguna', 
-                    'Lapangan Tennis', 
-                    'Lapangan Bulutangkis', 
+                    'Gedung Serbaguna',
+                    'Lapangan Tennis',
+                    'Lapangan Bulutangkis',
                     'TULT'
                   ].map<DropdownMenuItem<String>>((String value) {
                     return DropdownMenuItem<String>(
@@ -53,7 +58,10 @@ class DropdownRentWidget extends StatelessWidget {
                         value,
                         style: TextStyle(
                           fontSize: 16.0, // Samakan fontsize dengan deskripsi
-                          fontWeight: selectedValue == value ? FontWeight.bold : FontWeight.normal, // Teks yang dipilih menjadi tebal
+                          fontWeight: selectedValue == value
+                              ? FontWeight.bold
+                              : FontWeight
+                                  .normal, // Teks yang dipilih menjadi tebal
                         ),
                       ),
                     );
@@ -67,46 +75,62 @@ class DropdownRentWidget extends StatelessWidget {
           padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
           child: Container(
             padding: const EdgeInsets.all(20.0),
-            decoration: getImageRoundedWithShadowDecoration(selectedValue), // Menggunakan dekorasi gambar yang sesuai dengan nilai dropdown
+            decoration: getImageRoundedWithShadowDecoration(
+                selectedValue), // Menggunakan dekorasi gambar yang sesuai dengan nilai dropdown
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-Padding(
-  padding: const EdgeInsets.only(),
-  child: Row(
-    children: [
-    Expanded(
-        child: Container(
-          padding: const EdgeInsets.only(), // Padding di dalam kontainer
-          alignment: Alignment.centerRight,
-          child: Text(
-            'Rp$nominal', // Menampilkan nominal
-            textAlign: TextAlign.right, // Menempatkan teks ke kanan
-            style: const TextStyle(fontSize: 23.0, color: Colors.white, fontWeight: FontWeight.bold), // Warna putih, tebal, dan fontsize 16
-          ),
-        ),
-      ),
-    ],
-  ),
-),
-
+                Padding(
+                  padding: const EdgeInsets.only(),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets
+                              .only(), // Padding di dalam kontainer
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            'Rp$nominal', // Menampilkan nominal
+                            textAlign:
+                                TextAlign.right, // Menempatkan teks ke kanan
+                            style: const TextStyle(
+                                fontSize: 23.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight
+                                    .bold), // Warna putih, tebal, dan fontsize 16
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 const SizedBox(
                   height: 120.0,
                 ),
-                Text(  
+                Text(
                   subTitle, // Menampilkan subjudul
-                  style: const TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold), // Warna putih, tebal, dan fontsize 20
+                  style: const TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight
+                          .bold), // Warna putih, tebal, dan fontsize 20
                 ),
                 const SizedBox(
                   height: 10.0,
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(right: 70.0), // Padding hanya sebelah kanan
+                  padding: const EdgeInsets.only(
+                      right: 70.0), // Padding hanya sebelah kanan
                   child: Text(
                     description,
-                    textAlign: TextAlign.justify, // Teks diratakan kiri dan kanan
-                    style: const TextStyle(fontSize: 12.0, color: Colors.white, fontWeight: FontWeight.normal), // Warna putih, fontsize lebih kecil, dan tidak tebal
+                    textAlign:
+                        TextAlign.justify, // Teks diratakan kiri dan kanan
+                    style: const TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight
+                            .normal), // Warna putih, fontsize lebih kecil, dan tidak tebal
                   ),
                 ),
               ],
