ef0dfb4bc3d2c73671aa12215143f9f254f3bfb0
Law1911 <epensusanto@gmail.com>
2024-04-25 09:45:06 +0700

    update

--- rent_it_flutter/lib/main.dart
+++ rent_it_flutter/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:rent_it_flutter/page/home_page.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: SafeArea(child: PaymentPage()),
+      body: SafeArea(child: HomePage()),
     ),
   ));
 }
