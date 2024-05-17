5b3bde9e8870861826625c7488d3cabfee60c762
Foxxie1440 <90520474+ZhafranHafizh@users.noreply.github.com>
2024-05-17 21:44:49 +0700

    Make Profile Page

--- rent_it_flutter/lib/main.dart
+++ rent_it_flutter/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:rent_it_flutter/page/home_page.dart';
+import 'package:rent_it_flutter/page/landing_page.dart';
 import 'package:rent_it_flutter/page/payment_page.dart';
+import 'package:rent_it_flutter/page/profile_page.dart';
 // import 'package:rent_it_flutter/page/home_page.dart';
 // import 'package:rent_it_flutter/page/login_page.dart';
 
@@ -11,8 +13,8 @@ void main() {
         iconTheme: IconThemeData(color: Color.fromRGBO(217, 217, 217, 1)),
       ),
     ),
-    home: const Scaffold(
-      body: SafeArea(child: HomePage()),
+    home: Scaffold(
+      body: SafeArea(child: ProfilePage()), // Hapus const dari sini
     ),
   ));
 }
