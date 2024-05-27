758da7672e80e26e83ccc66df0c0e3a9d86b107b
SuryaaAulia <suryaaulia489@gmail.com>
2024-05-28 01:44:49 +0700

    Create - Facility Model
    model for reusability

---
+++ rent_it_flutter/lib/models/facility.dart
@@ -0,0 +1,22 @@
+class Facility {
+  final String image;
+  final String name;
+  final String description;
+  final int price;
+
+  Facility({
+    required this.image,
+    required this.name,
+    required this.description,
+    required this.price,
+  });
+
+  factory Facility.fromJson(Map<String, dynamic> json) {
+    return Facility(
+      image: json['image'],
+      name: json['name'],
+      description: json['description'],
+      price: (json['price']),
+    );
+  }
+}
