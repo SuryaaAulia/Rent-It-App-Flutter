06a4630acc82f28acc4933cf10a95e2dd188df64
SuryaaAulia <suryaaulia489@gmail.com>
2024-06-03 10:20:47 +0700

    Update - Facility service
    Generalize the service method, so I will be reuseable

--- rent_it_flutter/lib/services/facility_service.dart
+++ rent_it_flutter/lib/services/facility_service.dart
@@ -1,24 +1,14 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import '/models/facility.dart';
+import 'package:rent_it_flutter/models/facility.dart';
+import 'package:rent_it_flutter/services/service.dart';
 
 class FacilityService {
   static const String url = 'http://10.0.2.2:8000/api/facilities';
+  final Service<Facility> _service = Service<Facility>(
+    url: url,
+    fromJson: (json) => Facility.fromJson(json),
+  );
 
   Future<List<Facility>> fetchFacilities() async {
-    try {
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        List<dynamic> body = jsonDecode(response.body);
-        List<Facility> facilities =
-            body.map((dynamic item) => Facility.fromJson(item)).toList();
-        return facilities;
-      } else {
-        throw Exception(
-            'Failed to load facilities. Status code: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to load facilities. Error: $e');
-    }
+    return await _service.fetchItems();
   }
 }
