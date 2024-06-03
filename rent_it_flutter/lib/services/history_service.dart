9cd0e8644c368513cf46ec63bd6273dd01d11b97
SuryaaAulia <suryaaulia489@gmail.com>
2024-06-03 10:21:27 +0700

    Create - History Service
    History service to fetch the data from API api/histories

---
+++ rent_it_flutter/lib/services/history_service.dart
@@ -0,0 +1,14 @@
+import 'package:rent_it_flutter/models/history.dart'; // Import Facility model
+import 'package:rent_it_flutter/services/service.dart';
+
+class HistoryService {
+  static const String url = 'http://10.0.2.2:8000/api/histories';
+  final Service<History> _service = Service<History>(
+    url: url,
+    fromJson: (json) => History.fromJson(json),
+  );
+
+  Future<List<History>> fetchHistory() async {
+    return await _service.fetchItems();
+  }
+}
