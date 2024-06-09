b190578f626151b9b213606d9ff911e88e94c977
SuryaaAulia <suryaaulia489@gmail.com>
2024-06-10 01:50:22 +0700

    Update - History Service
    Change the url to the actual web API instead of local

--- rent_it_flutter/lib/services/history_service.dart
+++ rent_it_flutter/lib/services/history_service.dart
@@ -2,7 +2,7 @@ import 'package:rent_it_flutter/models/history.dart'; // Import Facility model
 import 'package:rent_it_flutter/services/service.dart';
 
 class HistoryService {
-  static const String url = 'http://10.0.2.2:8000/api/histories';
+  static const String url = 'https://rent-it.site/api/histories';
   final Service<History> _service = Service<History>(
     url: url,
     fromJson: (json) => History.fromJson(json),
