684343d7ec7154d8ecaa78c4bc0005decf967914
SuryaaAulia <suryaaulia489@gmail.com>
2024-06-09 11:29:43 +0700

    Update - appbar widget
    Add shimmer effect when load the data

--- rent_it_flutter/lib/widgets/appbar_widget.dart
+++ rent_it_flutter/lib/widgets/appbar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rent_it_flutter/pages/profile_page.dart';
+import 'package:rent_it_flutter/widgets/shimmer_widget.dart';
 
 class RAppBar extends StatelessWidget {
   const RAppBar({super.key});
@@ -10,19 +11,31 @@ class RAppBar extends StatelessWidget {
       padding: const EdgeInsets.fromLTRB(5, 35, 5, 20),
       child: AppBar(
         backgroundColor: Colors.transparent,
-        title: const Column(
+        title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               'Selamat Siang',
               style: TextStyle(
                   fontSize: 20, color: Color.fromRGBO(236, 232, 232, 1)),
             ),
-            Text('Stevent',
-                style: TextStyle(
-                    fontSize: 32,
-                    color: Color.fromRGBO(236, 232, 232, 1),
-                    fontWeight: FontWeight.bold))
+            FutureBuilder<String>(
+              future:
+                  Future.delayed(const Duration(seconds: 2), () => 'Stevent'),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const RShimmerWidget.rectangle(width: 200, height: 30);
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return Text(snapshot.data!,
+                      style: const TextStyle(
+                          fontSize: 32,
+                          color: Color.fromRGBO(236, 232, 232, 1),
+                          fontWeight: FontWeight.bold));
+                }
+              },
+            )
           ],
         ),
         actions: [
@@ -33,10 +46,25 @@ class RAppBar extends StatelessWidget {
                 MaterialPageRoute(builder: (context) => const ProfilePage()),
               );
             },
-            child: const CircleAvatar(
-              radius: 45,
-              backgroundImage: AssetImage('assets/images/avatar.jpg'),
-              backgroundColor: Colors.black,
+            child: FutureBuilder<String>(
+              future: Future.delayed(
+                  const Duration(seconds: 2), () => 'assets/images/avatar.jpg'),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircleAvatar(
+                    radius: 45,
+                    backgroundColor: Colors.grey[400],
+                  );
+                } else if (snapshot.hasError) {
+                  return const Icon(Icons.error);
+                } else {
+                  return CircleAvatar(
+                    radius: 45,
+                    backgroundImage: AssetImage(snapshot.data!),
+                    backgroundColor: Colors.grey[400],
+                  );
+                }
+              },
             ),
           ),
         ],
