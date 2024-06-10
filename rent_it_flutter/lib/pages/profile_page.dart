de72b096bb5fdd8c3a6bb706f44de8f2f5d6d2a0
SuryaaAulia <suryaaulia489@gmail.com>
2024-06-10 09:02:03 +0700

    Update

--- rent_it_flutter/lib/pages/profile_page.dart
+++ rent_it_flutter/lib/pages/profile_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:io';
-import 'edit_profile_page.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:local_auth/local_auth.dart'; // Import package local_auth
