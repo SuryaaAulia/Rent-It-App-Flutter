f274793ce147d96f655d1ceee433115d48dc9a30
SuryaaAulia <suryaaulia489@gmail.com>
2024-06-10 08:27:42 +0700

    Update - use web API

--- rent_it_flutter/lib/services/facility_service.dart
+++ rent_it_flutter/lib/services/facility_service.dart
@@ -2,7 +2,7 @@ import 'package:rent_it_flutter/models/facility.dart';
 import 'package:rent_it_flutter/services/service.dart';
 
 class FacilityService {
-  static const String url = 'http://10.0.2.2:8000/api/facilities';
+  static const String url = 'https://rent-it.site/api/facilities';
   final Service<Facility> _service = Service<Facility>(
     url: url,
     fromJson: (json) => Facility.fromJson(json),
